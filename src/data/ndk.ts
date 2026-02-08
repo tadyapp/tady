95b9d8a886d79d7bfccefa2df209215a9da56877
mrkvon <mrkvon@protonmail.com>
2026-02-08 14:38:41 +0100

    feat(pwa): add NDK cache adapter
    to show events when offline

--- src/data/ndk.ts
+++ src/data/ndk.ts
@@ -1,8 +1,14 @@
-// Import the package
+// TODO replace with @nostr-dev-kit/cache-dexie after https://github.com/nostr-dev-kit/ndk/pull/377 gets published
+import NDKCacheAdapterDexie from '@mrkvon/ndk-cache-dexie'
 import NDK from '@nostr-dev-kit/ndk'
+
+const cacheAdapter = new NDKCacheAdapterDexie({
+  dbName: 'tady-app',
+})
 
 // Create a new NDK instance with explicit relays
 export const ndk = new NDK({
+  cacheAdapter,
   explicitRelayUrls: ['wss://nos.lol'],
 })
 
