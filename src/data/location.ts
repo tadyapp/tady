7449821ba58af1278aa1c7873a10bb879a6a1f99
mrkvon <mrkvon@protonmail.com>
2026-02-16 15:46:15 +0100

    feat(location): improve select location interface

--- src/data/location.ts
+++ src/data/location.ts
@@ -3,3 +3,5 @@ import type { LatLng } from 'leaflet'
 
 export const locationSelected = signal<LatLng | undefined>(undefined)
 export const locationAuto = signal<LatLng | undefined>(undefined)
+export const locationPreference = signal<'auto' | 'manual'>('auto') // if both are available, preference is used, otherwise the one available is used
+export const radius = signal<number>(10000) // metres from origin
